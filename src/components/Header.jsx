1387407df5eb54543ce4e2e116cbe1fc5f2b3b67
Sadeen Otaibi <sadeenotaibi216@gmail.com>
2026-07-05 13:41:59 +0300

    fixedcomments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,34 +10,44 @@ function Button({ children, className = "", onClick }) {
 }
 
 function Header() {
+  const navButtons = ["Home", "TV Shows", "Movies", "Kids", "My List"];
+
+  const actionButtons = [
+    {
+      text: "🔍",
+      className: "text-xl",
+    },
+    {
+      text: "Sign In",
+      className: "border border-gray-600 px-5 py-2 rounded-lg hover:border-white",
+    },
+    {
+      text: "Start Free Trial",
+      className:
+        "bg-green-400 text-black px-5 py-2 rounded-lg hover:bg-green-300 hover:text-black",
+    },
+  ];
+
   return (
     <div className="bg-black text-white flex items-center justify-between px-8 py-4">
-      
       <div className="flex items-center gap-2">
         <img src="/logo.png" alt="Streamify-logo" className="w-10 h-10" />
         <span className="text-2xl font-semibold font-sans">Streamify</span>
       </div>
 
-      <div className="flex items-center gap-6 text-l">
-        <Button>Home</Button>
-        <Button>TV Shows</Button>
-        <Button>Movies</Button>
-        <Button>Kids</Button>
-        <Button>My List</Button>
+      <div className="flex items-center gap-6 text-lg">
+        {navButtons.map((button) => (
+          <Button key={button}>{button}</Button>
+        ))}
       </div>
 
       <div className="flex items-center gap-4 text-sm">
-        <Button className="text-xl">🔍</Button>
-
-        <Button className="border border-gray-600 px-5 py-2 rounded-lg hover:border-white">
-          Sign In
-        </Button>
-
-        <Button className="bg-green-400 text-black px-5 py-2 rounded-lg hover:bg-green-300 hover:text-black">
-          Start Free Trial
-        </Button>
+        {actionButtons.map((button) => (
+          <Button key={button.text} className={button.className}>
+            {button.text}
+          </Button>
+        ))}
       </div>
-
     </div>
   );
 }
