af4853a691f6960283eb447bbfc07fca2c234966
Sadeen Otaibi <sadeenotaibi216@gmail.com>
2026-07-06 11:16:54 +0300

    fixedtherror

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Cards from "./components/Cards";
 import Header from "./components/Header";
-import Footer from "./components/footer";
+
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
        <div className="min-h-screen bg-black text-white">
       <Header />
       <Cards/>
-   <Footer/>
+
     </div>
     </>
   );
