9fbd6262434a1ba1aaf1ea0cf4b6a5182b6a8844
Sadeen Otaibi <sadeenotaibi216@gmail.com>
2026-08-19 13:13:47 +0300

    testing -lastVersion

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -1,6 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import "@testing-library/react/dont-cleanup-after-each";
+import { cleanup, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
-import { describe, expect, test } from "vitest";
+import {
+  afterAll,
+  beforeAll,
+  describe,
+  expect,
+  test,
+} from "vitest";
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router";
 
@@ -8,7 +15,7 @@ import Footer from "./Footer";
 import { store } from "../store/Store";
 
 describe("Footer", () => {
-  test(" Streamify title", () => {
+  beforeAll(() => {
     render(
       <Provider store={store}>
         <MemoryRouter>
@@ -16,19 +23,17 @@ describe("Footer", () => {
         </MemoryRouter>
       </Provider>
     );
+  });
 
+  afterAll(() => {
+    cleanup();
+  });
+
+  test("Streamify title", () => {
     expect(screen.getByText("Streamify")).toBeInTheDocument();
   });
 
-  test(" description", () => {
-    render(
-      <Provider store={store}>
-        <MemoryRouter>
-          <Footer />
-        </MemoryRouter>
-      </Provider>
-    );
-
+  test("description", () => {
     expect(
       screen.getByText(/your favorite stories, all in one place/i)
     ).toBeInTheDocument();
@@ -38,59 +43,27 @@ describe("Footer", () => {
     ).toBeInTheDocument();
   });
 
-  test(" footer sections ", () => {
-    render(
-      <Provider store={store}>
-        <MemoryRouter>
-          <Footer />
-        </MemoryRouter>
-      </Provider>
-    );
-
+  test("footer sections", () => {
     expect(screen.getByText("Browse")).toBeInTheDocument();
     expect(screen.getByText("Company")).toBeInTheDocument();
     expect(screen.getByText("Support")).toBeInTheDocument();
   });
 
   test("shows Browse links", () => {
-    render(
-      <Provider store={store}>
-        <MemoryRouter>
-          <Footer />
-        </MemoryRouter>
-      </Provider>
-    );
-
     expect(screen.getByText("TV Shows")).toBeInTheDocument();
     expect(screen.getByText("Movies")).toBeInTheDocument();
     expect(screen.getByText("Kids")).toBeInTheDocument();
     expect(screen.getByText("My List")).toBeInTheDocument();
   });
 
-  test(" Company links", () => {
-    render(
-      <Provider store={store}>
-        <MemoryRouter>
-          <Footer />
-        </MemoryRouter>
-      </Provider>
-    );
-
+  test("Company links", () => {
     expect(screen.getByText("About Us")).toBeInTheDocument();
     expect(screen.getByText("Careers")).toBeInTheDocument();
     expect(screen.getByText("Press")).toBeInTheDocument();
     expect(screen.getByText("Contact Us")).toBeInTheDocument();
   });
 
-  test(" Support links", () => {
-    render(
-      <Provider store={store}>
-        <MemoryRouter>
-          <Footer />
-        </MemoryRouter>
-      </Provider>
-    );
-
+  test("Support links", () => {
     expect(screen.getByText("Help Center")).toBeInTheDocument();
     expect(screen.getByText("Terms of Use")).toBeInTheDocument();
     expect(screen.getByText("Privacy Policy")).toBeInTheDocument();
